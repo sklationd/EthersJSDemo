f36b73fe8f4cbf1e773c5c4cabd2ac4e8305bfc8
김선규 <skla@flysher.net>
2022-05-03 17:46:54 +0900

    add: get eth balacne feature

--- index.js
+++ index.js
@@ -1,12 +1,36 @@
-document.addEventListener("DOMContentLoaded", function () {
-  let connected = false;
+let provider = null;
+let signer = null;
+let wallet = null;
 
-  const connectButton = document.getElementById("connect-button");
-  connectButton.addEventListener("click", async function () {
-    const provider = new ethers.providers.Web3Provider(window.ethereum, "any");
+const load = async function () {
+  // METAMASK
+  if (window.ethereum) {
+    provider = new ethers.providers.Web3Provider(window.ethereum);
     await provider.send("eth_requestAccounts", []);
-    const signer = provider.getSigner();
-    let userAddress = await signer.getAddress();
-    console.log(userAddress);
-  });
+    signer = provider.getSigner();
+    wallet = await signer.getAddress();
+    console.log("load provider, signer, wallet doen");
+  } else {
+    alert("Please install metamask");
+  }
+};
+
+const refreshBalance = async function () {
+  if (!provider || !wallet) {
+    await load();
+  }
+  const balance = await provider.getBalance(wallet);
+  const balanceDiv = document.getElementById("eth-balance");
+  balanceDiv.innerText = balance / ethers.constants.WeiPerEther + " ETH";
+  console.log("Refresh Clicked");
+};
+
+const connectButton = document.getElementById("connect-button");
+connectButton.addEventListener("click", load);
+const refreshBalanceButton = document.getElementById("refresh-balance-button");
+refreshBalanceButton.addEventListener("click", refreshBalance);
+
+document.addEventListener("DOMContentLoaded", async function () {
+  await load();
+  await refreshBalance();
 });
